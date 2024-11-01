668deac8085a25ea6ade1c2f543c9c9246e78918
Haseebraza12 <munibhaseeb2@gmail.com>
2024-11-01 09:37:47 +0500

    app

---
+++ restaurantly/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Menu from '../components/Menu';
+import Specials from '../components/Specials';
+import Events from '../components/Events';
+import Chefs from '../components/Chefs';
+import Contact from '../components/Contact';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Menu />
+      <Specials />
+      <Events />
+      <Chefs />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
